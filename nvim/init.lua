06e3dfa69774b0588a57f24238a535fc288ee7b8
Hippo5412 <parkerlane003@gmail.com>
2026-01-31 13:30:32 -0600

    switched to blink.cmp and intergrated copilot into autocomplete

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,18 +17,20 @@ vim.pack.add({
 	{src = "https://github.com/nvim-treesitter/nvim-treesitter"},
 	{src = "https://github.com/mason-org/mason.nvim"},
 	{src = "https://github.com/mason-org/mason-lspconfig.nvim"},
-	{src = "https://github.com/echasnovski/mini.nvim"},
-	{src = "https://github.com/github/copilot.vim"},
+	{src = "https://github.com/Saghen/blink.cmp"},
+	{src = "https://github.com/zbirenbuam/copilot.lua"},
+	{src = "https://github.com/fang2hou/blink-copilot"},
 })
 
 vim.cmd("colorscheme pixel")
 require('transparent').setup({})
 
-require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason").setup({})
+require("mason-lspconfig").setup({})
 
-require('mini.snippets').setup({})
-require('mini.completion').setup({})
+require("blink.cmp").setup({fuzzy = { implementation = "lua"},sources = {default = { 'copilot', 'lsp', 'buffer', 'snippets', 'path' }, providers = {copilot = {name = "copilot",module = "blink-copilot",score_offset = 100,async = true,opts = {max_completions = 3,}}}}})
+
+require("copilot").setup({})
 
 vim.keymap.set('n', '<leader>e', ':Explore<CR>')
 local builtin = require('telescope.builtin')
